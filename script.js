9bc5c873a1b1cb228ac8ebca367c9e8ce0610e40
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-01-17 09:34:24 -0700

    Add addHoverability function

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-const grid = document.querySelector('.container');
+addHoverability();
+const grid = document.querySelector(".container");
 
-function resizeGrid(grid, sizeSelection){
-    const arr = [4,6,8,12,16,24];
-    const arr2 = ['fourGrid','sixGrid','eightGrid','twelveGrid','sixteenGrid','twentyFourGrid'];
+function resizeGrid(grid, sizeSelection) {
+  const arr = [4, 6, 8, 12, 16, 24];
+  const arr2 = [
+    "fourGrid",
+    "sixGrid",
+    "eightGrid",
+    "twelveGrid",
+    "sixteenGrid",
+    "twentyFourGrid",
+  ];
 
-    const elementStartCount = grid.childElementCount;
-    const increase = elementStartCount < arr[sizeSelection]**2 ? true : false;
+  const elementStartCount = grid.childElementCount;
+  const increase = elementStartCount < arr[sizeSelection] ** 2 ? true : false;
 
-    for(let i = 0; i<Math.abs(elementStartCount - arr[sizeSelection]**2); i++){
-        if(increase){
-            let div = document.createElement('div');
-            div.classList.add('item');
-            grid.appendChild(div);
-        } else {
-            grid.removeChild(grid.lastChild);
-        }
-    } 
-    grid.classList.remove(grid.classList[1]);
-    grid.classList.add(arr2[sizeSelection]);
+  for (
+    let i = 0;
+    i < Math.abs(elementStartCount - arr[sizeSelection] ** 2);
+    i++
+  ) {
+    if (increase) {
+      let div = document.createElement("div");
+      div.classList.add("item");
+      grid.appendChild(div);
+    } else {
+      grid.removeChild(grid.lastChild);
+    }
+  }
+  grid.classList.remove(grid.classList[1]);
+  grid.classList.add(arr2[sizeSelection]);
+  addHoverability();
 }
 
 const resizeButtons = document.querySelectorAll(".resize-button");
-resizeButtons.forEach((currentValue, i)=>{
-    currentValue.addEventListener("click", ()=>{
-        resizeGrid(grid, i);
-    });
+resizeButtons.forEach((currentValue, i) => {
+  currentValue.addEventListener("click", () => {
+    resizeGrid(grid, i);
+  });
 });
 
+function addHoverability() {
+const colorTo = "black";
+
+  const gridItems = document.querySelectorAll(".item");
+  gridItems.forEach((currentValue) => {
+    currentValue.addEventListener("mouseover", (e) => {
+      currentValue.style.backgroundColor = colorTo;
+    });
+  });
+}
