dfefa7f0c8e6063065a9f61fa10c6da9d0bee6e2
Darwin Diaz <darwin.diaz.ut@gmail.com>
2024-01-21 14:01:50 -0700

    Setup color buttons and fix color freeze bug

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-addHoverability();
 const grid = document.querySelector(".container");
+
+const black = ['black'];
+const warmColors = ['#FFBB5C','#FF9B50','#E25E3E','#C63D2F'];
+const coldColors = ['#DCF2F1','#7FC7D9','#365486','#0F1035'];
+
+let selectedPallet = warmColors;
+addHoverability(selectedPallet);
 
 function resizeGrid(grid, sizeSelection) {
   const arr = [4, 6, 8, 12, 16, 24];
@@ -30,8 +36,8 @@ function resizeGrid(grid, sizeSelection) {
   }
   grid.classList.remove(grid.classList[1]);
   grid.classList.add(arr2[sizeSelection]);
-  addHoverability();
   clearBoard();
+  addHoverability(selectedPallet);
 }
 
 const resizeButtons = document.querySelectorAll(".resize-button");
@@ -41,23 +47,35 @@ resizeButtons.forEach((currentValue, i) => {
   });
 });
 
-function addHoverability() {
-    const colorTo = "black";
+function addHoverability(colorArr) {
+  let index = 0;
 
   const gridItems = document.querySelectorAll(".item");
   gridItems.forEach((currentValue) => {
     currentValue.addEventListener("mouseover", (e) => {
+      colorTo = colorArr[index];
       currentValue.style.backgroundColor = colorTo;
+
+      index = (index + 1) % colorArr.length;
+      console.log(index);
+      console.log(colorArr[index]);
     });
   });
 }
-
 function clearBoard(){
     const gridItems = document.querySelectorAll(".item");
   gridItems.forEach((currentValue) => {
     currentValue.style.backgroundColor = '';
   });
 }
-
 const clearButton = document.querySelector('.clear-button');
 clearButton.addEventListener("click", clearBoard);
+
+let colors = [black, warmColors, coldColors]
+const colorButtons = document.querySelectorAll(".color-button");
+colorButtons.forEach((currentValue, i) => {
+  currentValue.addEventListener("click", () => {
+    selectedPallet = colors[i]
+    addHoverability(selectedPallet);
+  });
+});
